aca528f41c49f4ea63e6ee6d130542d8c51f5152
ericstevens <eric@ericstevens.ca>
2018-01-03 09:26:06 -0500

    Added TEST based full print of response and json

--- creativecheck.py
+++ creativecheck.py
@@ -75,6 +75,10 @@ def querycreative(u: str, a):
         msg.ERROR("Connection timeout Error")
     except requests.exceptions.RequestException as e:
         msg.ERROR(e)
+    if arg.Flags.test:
+        msg.TEST("full json is \n\t{}".format(json.loads(r.content.decode('utf-8'))))
+        msg.TEST("\n\tstatus: {}\n\theaders: {}\n\turl: {}\n\treason: {}".format(r.status_code, r.headers, r.url, r.reason))
+
     return r.status_code, json.loads(r.content.decode('utf-8'))
 
 
